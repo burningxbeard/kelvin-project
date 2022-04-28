9d862e9212b8627cd1bcc2846fe0d8395910ace5
burningxbeard <102481202+burningxbeard@users.noreply.github.com>
2022-04-27 21:57:43 -0400

    Update new-main.js

--- new-main.js
+++ new-main.js
@@ -1,8 +1,9 @@
 // Starting temperature for our calculation 
 const kelvin = 300;
 // Converting kelvin to celsius 
-var celsius = (kelvin - 273);
+const celsius = (kelvin - 273);
 // Converting celsius to farenheit and round down 
-var fahrenheit = Math.floor(celsius * (9/5) + 32);
-
-console.log(`The temperature is ${fahrenheit} degrees farenheit`);
+const fahrenheit = Math.floor(celsius * (9/5) + 32);
+// include newton as an option 
+const newton = Math.floor(celsius * (33/100));
+console.log(`The temperature is ${fahrenheit} degrees farenheit which is also ${newton} newton?`);
